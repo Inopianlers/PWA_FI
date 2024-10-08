5f9c012e882406f6a5a2a1e57033996752912f33
Inopianlers <101866570+Inopianlers@users.noreply.github.com>
2024-10-08 12:50:42 +0800

    update path javascript

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,11 @@
 ﻿if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./service-worker.js') 
+        navigator.serviceWorker.register('./JS/service-worker.js')  
             .then((registration) => {
-                console.log('Service Worker registered with scope:', registration.scope);
+                console.log('ServiceWorker registered with scope:', registration.scope);
             })
             .catch((error) => {
-                console.error('Service Worker registration failed:', error);
+                console.error('ServiceWorker registration failed:', error);
             });
     });
 }
